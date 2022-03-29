637de7c431908479ee7faa9d89378e6c646bab9f
tutruongmugi <khongthenho304@gmail.com>
2022-03-29 20:37:07 +0700

    Header menu Realtors

--- src/components/common/FullControlledHeader.tsx
+++ src/components/common/FullControlledHeader.tsx
@@ -1,65 +1,115 @@
-import { Menu } from "@mui/icons-material";
 import {
   AppBar,
-  Avatar,
   Hidden,
   IconButton,
   Toolbar,
   Typography,
+  Link,
+  Button,
+  MenuItem,
+  Menu,
+  Paper,
 } from "@mui/material";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { Box } from "@mui/system";
-import Link from "next/link";
+import NextLink from "next/link";
+import { useState } from "react";
+import useStyles from "../../utils/styles";
+import { menuItems } from "../../utils/data";
 
 export default function FullControlledHeader() {
+  const classes = useStyles();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const onClickHandler = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+  const onMenuCloseHandler = () => {
+    setAnchorEl(null);
+  };
   return (
     <AppBar position="sticky" style={{ top: 0 }}>
       <Toolbar
         style={{
           display: "flex",
           justifyContent: "space-between",
-          alignItems: "center",
         }}
       >
-        <Box display="flex" alignItems="center">
-          <Avatar
-            src="https://avatars.githubusercontent.com/u/25724101?s=40&v=4"
-            alt="hello"
-          />
-
-          <Typography
-            variant="h6"
-            component="h1"
-            style={{ fontWeight: 600, marginLeft: 16 }}
-          >
-            User Factory Page
-          </Typography>
-        </Box>
+        <NextLink href="/" passHref>
+          <Link>
+            <Typography className={classes.brand}>Realtor.com</Typography>
+          </Link>
+        </NextLink>
 
         <Hidden smDown>
-          <Box display="flex" alignItems="center">
-            <Box display="flex" alignItems="center">
-              <Link href="/home" passHref>
-                <a style={{ color: "white", marginRight: 16 }}>Home</a>
-              </Link>
-              <Link href="/about-us" passHref>
-                <a style={{ color: "white", marginRight: 16 }}>About us</a>
-              </Link>
-            </Box>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            {menuItems.map((item) => (
+              <div key={item.title}>
+                <Typography
+                  className={classes.menuItem} 
+                  onMouseOver={onClickHandler} 
+                >
+                  {item.title}
+                </Typography>
+                <Menu
+                  id="basic-menu"
+                  anchorEl={anchorEl}
+                  open={open}
+                  onClose={onMenuCloseHandler}
+                  MenuListProps={{
+                    "aria-labelledby": "basic-button",
+                  }}
+                >
+                  <MenuItem onClick={onMenuCloseHandler}>
+                    Homes for sale
+                  </MenuItem>
+                  <MenuItem onClick={onMenuCloseHandler}>
+                    New Home Construction
+                  </MenuItem>
+                </Menu>
+              </div>
+            ))}
+          </div>
 
-            <Box display="flex" alignItems="center">
-              <Link href="/login" passHref>
-                <a style={{ color: "white", marginRight: 16 }}>Login</a>
-              </Link>
-              <Link href="/sign-up" passHref>
-                <a style={{ color: "white" }}>Sign up</a>
-              </Link>
-            </Box>
-          </Box>
+          <div>
+            <Button
+              variant="contained"
+              color="primary"
+              className={classes.buttonHeader}
+            >
+              <Typography className={classes.grow}>Manage rentals</Typography>
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              className={classes.buttonHeader}
+            >
+              <Typography className={classes.grow}>Advertise</Typography>
+            </Button>
+          </div>
+          <div>
+            <NextLink href="/login" passHref>
+              <Link className={classes.grow}>Log in</Link>
+            </NextLink>
+            <Button
+              variant="contained"
+              color="secondary"
+              style={{ marginLeft: 20, borderRadius: 36 }}
+            >
+              <Typography>Sign up</Typography>
+            </Button>
+          </div>
         </Hidden>
 
         <Hidden smUp>
           <IconButton>
-            <Menu style={{ color: "white" }} />
+            <AccountCircleIcon></AccountCircleIcon>
           </IconButton>
         </Hidden>
       </Toolbar>
