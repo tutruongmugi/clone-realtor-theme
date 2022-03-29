9753572ffbb2221754852c07626fb0ee334f5666
tutruongmugi <khongthenho304@gmail.com>
2022-03-29 20:35:39 +0700

    Add style

--- src/utils/theme.tsx
+++ src/utils/theme.tsx
@@ -3,7 +3,11 @@ import { createTheme } from "@mui/material";
 export const theme = createTheme({
   palette: {
     primary: {
-      main: "#1e88e5",
+      main: "#fff",
     },
+    secondary:{
+      main:"#D92228"
+    }
   },
+  spacing:42
 });
