75a07d5ca74f5eea5db672b550a82b80dd7cccf1
letuthptnguyendueduvn@gmail.comclear <letuthptnguyendueduvn@gmail.com>
2022-03-29 23:16:10 +0700

    Fix: Update theme

--- src/utils/theme.tsx
+++ src/utils/theme.tsx
@@ -3,11 +3,41 @@ import { createTheme } from "@mui/material";
 export const theme = createTheme({
   palette: {
     primary: {
-      main: "#fff",
+      main: "#D92228",
     },
-    secondary:{
-      main:"#D92228"
-    }
+    secondary: {
+      main: "#D92228",
+    },
   },
-  spacing:42
+  spacing: 42,
+  components: {
+    MuiLink: {
+      styleOverrides: {
+        root: {
+          color: "rgb(51, 51, 51)",
+          textDecoration: "none",
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: 99,
+          textTransform: "none",
+        },
+      },
+    },
+    MuiContainer: {
+      styleOverrides: {
+        root: {
+          paddingLeft: 16,
+          paddingRight: 16,
+          "@media (min-width: 600px)": {
+            paddingLeft: 16,
+            paddingRight: 16,
+          },
+        },
+      },
+    },
+  },
 });
